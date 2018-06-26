a7469f024badfa146afba3acabad65673a1e07bb
Flora May Rowden <21frowden@nerinxhs.org>
2018-06-26 15:21:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 $(document).ready(function (){
 });
+
+let url =''
+
+function searchRepositories() {
+  const searchTerms = $('searchTerms').val()
+  $.get ('application/vnd.github.mercy-preview+json')
+    $('searchTerms'); data => {
+      $('#results').html();
+    }
+}
