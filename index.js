1b764142037fc06153fbf76324c80e022cfcba7c
Flora May Rowden <21frowden@nerinxhs.org>
2018-06-26 15:33:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ $(document).ready(function (){
 });
 
 
-function searchRepositories() {
-  const searchTerms = $('searchTerms').val()
-  $.get ('application/vnd.github.mercy-preview+json')
-    $('searchTerms'); data => {
-      $('#results').html();
-    }
-} 
+let renderSearchResult = (result) => {
+ return `
+ <div>
+ <h2><a href="${result.html_url}">${result.name}</a>
+ </h2>
+ <p><a href="#" data-repository= "${result.name}" data-owner= "${result.owner.login}" onclick = "showCommits(this)">showCommits</a>
+ </p>
+ `
+}
+
+let renderSearchResults = (data) => data.items.map(result=>renderSearchResult(result))
+
+let searchRepositories = () => {
+ const searchTerms = $('#searchTerms').val()
+ $.get(`https://api.github.com/search/repositories?q=${searchTerms}`,data=>{
+   $('#results').html(renderSearchResults(data))
+ }).fail(error=> {
+   displayError()}) ;
+}
